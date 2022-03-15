ebdd4d20eb1bd534879aae4e36aada9719f8f5ef
Daniel Wart <dman.wart@gmail.com>
2022-03-14 17:46:31 -0700

    byte array new() changes

--- gdext-builtin/src/packed_byte_array.rs
+++ gdext-builtin/src/packed_byte_array.rs
@@ -1,24 +1,32 @@
+use std::ptr::null_mut;
+
 use once_cell::sync::Lazy;
 use gdext_sys::{self as sys, interface_fn};
 use crate::PtrCallArg;
 
 #[repr(C, align(8))]
 #[derive(Clone, Debug)]
-pub struct PackedByteArray(pub Vec<u8>);
+pub struct PackedByteArray{
+    gd_native_ptr: sys::GDNativeTypePtr,
+    pub vec: Vec<u8>
+}
 
 impl PackedByteArray{
     #[doc(hidden)]
     pub fn as_mut_ptr(&mut self) -> sys::GDNativeObjectPtr {
-        self.0.as_mut_ptr() as *mut _
+        self.gd_native_ptr
     }
     #[doc(hidden)]
     pub fn as_ptr(&self) -> sys::GDNativeObjectPtr {
-        self.0.as_ptr() as *mut _
+        self.gd_native_ptr
     }
 
     pub fn new() -> Self {
         unsafe {
-            let mut s = Self(vec![]);
+            let mut byte_array = Self{
+                gd_native_ptr: null_mut(),
+                vec: vec![],
+            };
 
             static CONSTR: Lazy<
                 unsafe extern "C" fn(sys::GDNativeTypePtr, *const sys::GDNativeTypePtr),
@@ -29,8 +37,9 @@ impl PackedByteArray{
                 )
                     .unwrap()
             });
-            CONSTR(s.as_mut_ptr(), std::ptr::null());
-            s
+            let vec: sys::GDNativeTypePtr = byte_array.vec.as_mut_ptr() as _;
+            CONSTR(byte_array.as_mut_ptr(), &vec as _);
+            byte_array
         }
     }
 }
